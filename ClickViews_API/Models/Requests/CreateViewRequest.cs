b2fb4b8cf13d9a92a5ff866dc38de2c34510cac9
Christoph Döllinger <christoph.doellinger@med.uni-heidelberg.de>
2024-02-15 16:18:01 +0100

    .

--- ClickViews_API/Models/Requests/CreateViewRequest.cs
+++ ClickViews_API/Models/Requests/CreateViewRequest.cs
@@ -4,7 +4,7 @@ namespace ClickViews_API.Models
     {
         public required string Id { get; set; }
         public required string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public required string Database { get; set; }
         public required string Query { get; set; }
     }
