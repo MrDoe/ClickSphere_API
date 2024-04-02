63673d2de8f74544965ddadb921c638390b8d1a5
Christoph Döllinger <christoph.doellinger@med.uni-heidelberg.de>
2024-04-02 10:00:21 +0200

    Refactor code to improve performance and readability

--- ClickSphere_API/Controllers/TableController.cs
+++ ClickSphere_API/Controllers/TableController.cs
@@ -1,181 +1,177 @@
-using System.Text;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ClickSphere_API.Services;
-using ClickSphere_API.Models;
-
-namespace ClickSphere_API.Controllers
+using ClickSphere_API.Models.Requests;
+namespace ClickSphere_API.Controllers;
+/// <summary>
+/// The base class for ClickSphere API controllers.
+/// </summary>
+[ApiController]
+public class TableController(IDbService dbService) : ControllerBase
 {
+    private readonly IDbService _dbService = dbService;
+
     /// <summary>
-    /// The base class for ClickSphere API controllers.
+    /// Create a new table in the specified database
     /// </summary>
-    [ApiController]
-    public class TableController(IDbService dbService) : ControllerBase
+    /// <param name="database">The database where the table should be created</param>
+    /// <param name="tableName">The name of the table to create</param>
+    /// <param name="columns">The columns of the table in the format "column1 datatype, column2 datatype, ..."</param>
+    /// <returns>The result of the table creation</returns>
+    [Authorize]
+    [HttpPost]
+    [Route("/createTable")]
+    public async Task<IResult> CreateTable(CreateTableRequest request)
     {
-        private readonly IDbService _dbService = dbService;
+        string query = $"CREATE TABLE {request.Database}.{request.Table} " +
+                       $"({request.Columns}) " +
+                       $"ENGINE = {request.Engine}() ";
 
-        /// <summary>
-        /// Create a new table in the specified database
-        /// </summary>
-        /// <param name="database">The database where the table should be created</param>
-        /// <param name="tableName">The name of the table to create</param>
-        /// <param name="columns">The columns of the table in the format "column1 datatype, column2 datatype, ..."</param>
-        /// <returns>The result of the table creation</returns>
-        [Authorize]
-        [HttpPost]
-        [Route("/createTable")]
-        public async Task<IResult> CreateTable(CreateTableRequest request)
+        // add order by only if defined
+        if (!string.IsNullOrEmpty(request.OrderBy))
         {
-            string query = $"CREATE TABLE {request.Database}.{request.Table} " +
-                           $"({request.Columns}) " +
-                           $"ENGINE = {request.Engine}() ";
-            
-            // add order by only if defined
-            if(!string.IsNullOrEmpty(request.OrderBy))
-            {
-                query += $"ORDER BY ({request.OrderBy})";
-            }
-
-            query += ";";
-            
-            int result = await _dbService.ExecuteNonQuery(query);
-            
-            if (result == 0)
-                return Results.Ok();
-            else
-                return Results.BadRequest("Could not create table");
+            query += $"ORDER BY ({request.OrderBy})";
         }
 
-        /// <summary>
-        /// Delete a table from a database.
-        /// </summary>
-        /// <param name="database">The database where the table is located.</param>
-        /// <param name="table">The table to delete.</param>
-        /// <returns>The result of the table deletion.</returns>
-        [Authorize]
-        [HttpDelete]
-        [Route("/deleteTable")]
-        public async Task<IResult> DeleteTable(string database, string table)
+        query += ";";
+
+        int result = await _dbService.ExecuteNonQuery(query);
+
+        if (result == 0)
+            return Results.Ok();
+        else
+            return Results.BadRequest("Could not create table");
+    }
+
+    /// <summary>
+    /// Delete a table from a database.
+    /// </summary>
+    /// <param name="database">The database where the table is located.</param>
+    /// <param name="table">The table to delete.</param>
+    /// <returns>The result of the table deletion.</returns>
+    [Authorize]
+    [HttpDelete]
+    [Route("/deleteTable")]
+    public async Task<IResult> DeleteTable(string database, string table)
+    {
+        int result = await _dbService.ExecuteNonQuery($"DROP TABLE {database}.{table}");
+        if (result == 0)
+            return Results.Ok();
+        else
+            return Results.BadRequest("Could not drop table");
+    }
+
+    /// <summary>
+    /// Get the columns of a table.
+    /// </summary>
+    /// <param name="database">The database where the table is located.</param>
+    /// <param name="table">The table to get the columns from.</param>
+    /// <returns>The columns of the table.</returns>
+    [Authorize]
+    [HttpGet]
+    [Route("/getColumns")]
+    public async Task<IEnumerable<string>> GetColumns(string database, string table)
+    {
+        return await _dbService.ExecuteQuery($"DESCRIBE {database}.{table}");
+    }
+
+    /// <summary>
+    /// Get the first n rows from a table.
+    /// </summary>
+    /// <param name="database">The database where the table is located.</param>
+    /// <param name="table">The table to get the rows from.</param>
+    /// <param name="limit">The number of rows to get.</param>
+    /// <returns>The first n rows from the table.</returns>
+    [Authorize]
+    [HttpPost]
+    [Route("/getRows")]
+    public async Task<IEnumerable<Dictionary<string, object>>> CustomQuery([FromBody] GetRowsRequest request)
+    {
+        var sql = $"SELECT {request.Columns} FROM {request.Database}.{request.Table} ";
+
+        if (!string.IsNullOrEmpty(request.OrderBy))
         {
-            int result = await _dbService.ExecuteNonQuery($"DROP TABLE {database}.{table}");
-            if (result == 0)
-                return Results.Ok();
-            else
-                return Results.BadRequest("Could not drop table");
+            sql += $"ORDER BY {request.OrderBy} ";
         }
 
-        /// <summary>
-        /// Get the columns of a table.
-        /// </summary>
-        /// <param name="database">The database where the table is located.</param>
-        /// <param name="table">The table to get the columns from.</param>
-        /// <returns>The columns of the table.</returns>
-        [Authorize]
-        [HttpGet]
-        [Route("/getColumns")]
-        public async Task<IEnumerable<string>> GetColumns(string database, string table)
+        if (!string.IsNullOrEmpty(request.Limit))
         {
-            return await _dbService.ExecuteQuery($"DESCRIBE {database}.{table}");
+            sql += $"LIMIT {request.Limit}";
         }
 
-        /// <summary>
-        /// Get the first n rows from a table.
-        /// </summary>
-        /// <param name="database">The database where the table is located.</param>
-        /// <param name="table">The table to get the rows from.</param>
-        /// <param name="limit">The number of rows to get.</param>
-        /// <returns>The first n rows from the table.</returns>
-        [Authorize]
-        [HttpPost]
-        [Route("/getRows")]
-        public async Task<IEnumerable<Dictionary<string, object>>> CustomQuery([FromBody] GetRowsRequest request)
-        {
-            var sql = $"SELECT {request.Columns} FROM {request.Database}.{request.Table} ";
-            
-            if(!string.IsNullOrEmpty(request.OrderBy))
-            {
-                sql += $"ORDER BY {request.OrderBy} ";
-            }
+        sql += ";";
 
-            if(!string.IsNullOrEmpty(request.Limit))
-            {
-                sql += $"LIMIT {request.Limit}";
-            }
+        return await _dbService.ExecuteQueryDictionary(sql);
+    }
 
-            sql += ";";
+    /// <summary>
+    /// Insert a row into a table.
+    /// </summary>
+    /// <param name="database">The database where the table is located.</param>
+    /// <param name="table">The table to insert the row into.</param>
+    /// <param name="values">The values of the row in the format "value1, value2, ...".</param>
+    /// <returns>The result of the insertion.</returns>
+    [Authorize]
+    [HttpPost]
+    [Route("/insertRow")]
+    public async Task<IResult> InsertRow(InsertRowRequest request)
+    {
+        string query = $"INSERT INTO {request.Database}.{request.Table} " +
+                       $"VALUES ({request.Values});";
 
-            return await _dbService.ExecuteQueryDictionary(sql);
-        }
+        int result = await _dbService.ExecuteNonQuery(query);
 
-        /// <summary>
-        /// Insert a row into a table.
-        /// </summary>
-        /// <param name="database">The database where the table is located.</param>
-        /// <param name="table">The table to insert the row into.</param>
-        /// <param name="values">The values of the row in the format "value1, value2, ...".</param>
-        /// <returns>The result of the insertion.</returns>
-        [Authorize]
-        [HttpPost]
-        [Route("/insertRow")]
-        public async Task<IResult> InsertRow(InsertRowRequest request)
-        {
-            string query = $"INSERT INTO {request.Database}.{request.Table} " +
-                           $"VALUES ({request.Values});";
-            
-            int result = await _dbService.ExecuteNonQuery(query);
-            
-            if (result == 0)
-                return Results.Ok();
-            else
-                return Results.BadRequest("Could not insert row");
-        }
+        if (result == 0)
+            return Results.Ok();
+        else
+            return Results.BadRequest("Could not insert row");
+    }
 
-        /// <summary>
-        /// Update a row in a table.
-        /// </summary>
-        /// <param name="database">The database where the table is located.</param>
-        /// <param name="table">The table to update the row in.</param>
-        /// <param name="values">The new values of the row in the format "column1 = value1, column2 = value2, ...".</param>
-        /// <param name="condition">The condition that the row must satisfy.</param>
-        /// <returns>The result of the update.</returns>
-        [Authorize]
-        [HttpPost]
-        [Route("/updateRow")]
-        public async Task<IResult> UpdateRow(UpdateRowRequest request)
-        {
-            string query = $"UPDATE {request.Database}.{request.Table} " +
-                           $"SET {request.Values} " +
-                           $"WHERE {request.Condition};";
-            
-            int result = await _dbService.ExecuteNonQuery(query);
-            
-            if (result == 0)
-                return Results.Ok();
-            else
-                return Results.BadRequest("Could not update row");
-        }
+    /// <summary>
+    /// Update a row in a table.
+    /// </summary>
+    /// <param name="database">The database where the table is located.</param>
+    /// <param name="table">The table to update the row in.</param>
+    /// <param name="values">The new values of the row in the format "column1 = value1, column2 = value2, ...".</param>
+    /// <param name="condition">The condition that the row must satisfy.</param>
+    /// <returns>The result of the update.</returns>
+    [Authorize]
+    [HttpPost]
+    [Route("/updateRow")]
+    public async Task<IResult> UpdateRow(UpdateRowRequest request)
+    {
+        string query = $"UPDATE {request.Database}.{request.Table} " +
+                       $"SET {request.Values} " +
+                       $"WHERE {request.Condition};";
 
-        /// <summary>
-        /// Delete a row from a table.
-        /// </summary>
-        /// <param name="database">The database where the table is located.</param>
-        /// <param name="table">The table to delete the row from.</param>
-        /// <param name="condition">The condition that the row must satisfy.</param>
-        /// <returns>The result of the deletion.</returns>
-        [Authorize]
-        [HttpDelete]
-        [Route("/deleteRow")]
-        public async Task<IResult> DeleteRow(DeleteRowRequest request)
-        {
-            string query = $"DELETE FROM {request.Database}.{request.Table} " +
-                           $"WHERE {request.Condition};";
-            
-            int result = await _dbService.ExecuteNonQuery(query);
-            
-            if (result == 0)
-                return Results.Ok();
-            else
-                return Results.BadRequest("Could not delete row");
-        }
+        int result = await _dbService.ExecuteNonQuery(query);
+
+        if (result == 0)
+            return Results.Ok();
+        else
+            return Results.BadRequest("Could not update row");
+    }
+
+    /// <summary>
+    /// Delete a row from a table.
+    /// </summary>
+    /// <param name="database">The database where the table is located.</param>
+    /// <param name="table">The table to delete the row from.</param>
+    /// <param name="condition">The condition that the row must satisfy.</param>
+    /// <returns>The result of the deletion.</returns>
+    [Authorize]
+    [HttpDelete]
+    [Route("/deleteRow")]
+    public async Task<IResult> DeleteRow(DeleteRowRequest request)
+    {
+        string query = $"DELETE FROM {request.Database}.{request.Table} " +
+                       $"WHERE {request.Condition};";
+
+        int result = await _dbService.ExecuteNonQuery(query);
+
+        if (result == 0)
+            return Results.Ok();
+        else
+            return Results.BadRequest("Could not delete row");
     }
 }
--- ClickSphere_API/Controllers/UserController.cs
+++ ClickSphere_API/Controllers/UserController.cs
@@ -3,157 +3,155 @@ using Microsoft.AspNetCore.Mvc;
 using ClickSphere_API.Models;
 using ClickSphere_API.Services;
 using ClickSphere_API.Tools;
+using ClickSphere_API.Models.Requests;
 using Microsoft.AspNetCore.Authentication;
-
-namespace ClickSphere_API.Controllers
+namespace ClickSphere_API.Controllers;
+/// <summary>
+/// The base class for ClickSphere API controllers.
+/// </summary>
+[ApiController]
+public class UserController(IApiUserService userService) : ControllerBase
 {
+    private readonly IApiUserService _userService = userService;
+
     /// <summary>
-    /// The base class for ClickSphere API controllers.
+    /// This method is used to log in a user.
     /// </summary>
-    [ApiController]
-    public class UserController(IApiUserService userService) : ControllerBase
+    /// <param name="model">The user to log in.</param>
+    /// <returns>The result of the login.</returns>
+    [AllowAnonymous]
+    [HttpPost]
+    [Route("/login")]
+    public async Task<IResult> Login([FromBody] LoginRequest user)
     {
-        private readonly IApiUserService _userService = userService;
+        if (user == null)
+            return Results.BadRequest("User is required");
+        if (user.Username == null || user.Password == null)
+            return Results.BadRequest("Username and password are required");
 
-        /// <summary>
-        /// This method is used to log in a user.
-        /// </summary>
-        /// <param name="model">The user to log in.</param>
-        /// <returns>The result of the login.</returns>
-        [AllowAnonymous]
-        [HttpPost]
-        [Route("/login")]
-        public async Task<IResult> Login([FromBody] LoginRequest user)
+        var claimsPrincipal = await _userService.CheckLogin(user.Username, user.Password);
+
+        if (claimsPrincipal != null)
         {
-            if (user == null)
-                return Results.BadRequest("User is required");
-            if (user.Username == null || user.Password == null)
-                return Results.BadRequest("Username and password are required");
-
-            var claimsPrincipal = await _userService.CheckLogin(user.Username, user.Password);
-
-            if (claimsPrincipal != null)
+            var authProperties = new AuthenticationProperties
             {
-                var authProperties = new AuthenticationProperties
-                {
-                    IsPersistent = true,
-                    ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(30)
-                };
-                return Results.SignIn(claimsPrincipal, authProperties);
-            }
+                IsPersistent = true,
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(30)
+            };
+            return Results.SignIn(claimsPrincipal, authProperties);
+        }
+        else
+            return Results.BadRequest("Could not verify username and password");
+    }
+
+    /// <summary>
+    /// This method is used to log out a user.
+    /// </summary>
+    /// <returns>The result of the logout.</returns>
+    [Authorize]
+    [HttpPost]
+    [Route("/logout")]
+    public IResult Logout()
+    {
+        return Results.SignOut();
+    }
+
+    /// <summary>
+    /// Create new user in the ClickSphere users table
+    /// </summary>
+    /// <param name="model">The user to create</param>
+    /// <returns>The result of the creation</returns>
+    [Authorize]
+    [HttpPost]
+    [Route("/createUser")]
+    public async Task<IResult> CreateUser([FromBody] CreateUserRequest user)
+    {
+        if (user == null)
+            return Results.BadRequest("User is required");
+
+        if (string.IsNullOrEmpty(user.Username) || string.IsNullOrEmpty(user.Password))
+            return Results.BadRequest("Username and password are required");
+
+        Result result = await _userService.CreateUser(user.Username, user.Password);
+        if (result.IsSuccessful)
+            return Results.Ok();
+        else
+            return Results.BadRequest(result.Output);
+    }
+
+    /// <summary>
+    /// Delete a user from the ClickSphere users table
+    /// </summary>
+    /// <param name="username"></param>
+    /// <returns></returns>
+    [Authorize]
+    [HttpDelete]
+    [Route("/deleteUser")]
+    public async Task<IResult> DeleteUser(string username)
+    {
+        if (string.IsNullOrEmpty(username))
+            return Results.BadRequest("Username is required");
+
+        Result result = await _userService.DeleteUser(username);
+        if (result.IsSuccessful)
+            return Results.Ok();
+        else
+            return Results.BadRequest(result.Output);
+    }
+
+    /// <summary>
+    /// Get all users from the Users table
+    /// </summary>
+    /// <returns>The list of users</returns>
+    [Authorize]
+    [HttpGet]
+    [Route("/getUsers")]
+    public async Task<List<UserConfig>> GetUsers()
+    {
+        return await _userService.GetUsers();
+    }
+
+    /// <summary>
+    /// Get user configuration from ClickSphere.users
+    /// </summary>
+    /// <param name="userId">The user id of the user</param>
+    /// <returns>The user configuration</returns>
+    [Authorize]
+    [HttpGet]
+    [Route("/getUserConfig")]
+    public async Task<IResult?> GetUserConfig(string userId)
+    {
+        if (Guid.TryParse(userId, out Guid uid))
+        {
+            var result = await _userService.GetUserConfig(uid);
+            if (result != null)
+                return Results.Ok(result);
             else
-                return Results.BadRequest("Could not verify username and password");
+                return Results.BadRequest("User does not exist");
         }
-
-        /// <summary>
-        /// This method is used to log out a user.
-        /// </summary>
-        /// <returns>The result of the logout.</returns>
-        [Authorize]
-        [HttpPost]
-        [Route("/logout")]
-        public IResult Logout()
+        else
         {
-            return Results.SignOut();
+            return Results.BadRequest("User ID has to be a valid GUID");
         }
+    }
 
-        /// <summary>
-        /// Create new user in the ClickSphere users table
-        /// </summary>
-        /// <param name="model">The user to create</param>
-        /// <returns>The result of the creation</returns>
-        [Authorize]
-        [HttpPost]
-        [Route("/createUser")]
-        public async Task<IResult> CreateUser([FromBody] CreateUserRequest user)
-        {
-            if (user == null)
-                return Results.BadRequest("User is required");
+    /// <summary>
+    /// Update user configuration.
+    /// </summary>
+    /// <param name="user">The user configuration to update.</param>
+    /// <returns>The result of the update.</returns>
+    [Authorize]
+    [HttpPost]
+    [Route("/updateUser")]
+    public async Task<IResult> UpdateUser([FromBody] UserConfig user)
+    {
+        if (user == null)
+            return Results.BadRequest("User is required");
 
-            if (string.IsNullOrEmpty(user.Username) || string.IsNullOrEmpty(user.Password))
-                return Results.BadRequest("Username and password are required");
-
-            Result result = await _userService.CreateUser(user.Username, user.Password);
-            if (result.IsSuccessful)
-                return Results.Ok();
-            else
-                return Results.BadRequest(result.Output);
-        }
-
-        /// <summary>
-        /// Delete a user from the ClickSphere users table
-        /// </summary>
-        /// <param name="username"></param>
-        /// <returns></returns>
-        [Authorize]
-        [HttpDelete]
-        [Route("/deleteUser")]
-        public async Task<IResult> DeleteUser(string username)
-        {
-            if (string.IsNullOrEmpty(username))
-                return Results.BadRequest("Username is required");
-
-            Result result = await _userService.DeleteUser(username);
-            if (result.IsSuccessful)
-                return Results.Ok();
-            else
-                return Results.BadRequest(result.Output);
-        }
-
-        /// <summary>
-        /// Get all users from the Users table
-        /// </summary>
-        /// <returns>The list of users</returns>
-        [Authorize]
-        [HttpGet]
-        [Route("/getUsers")]
-        public async Task<List<UserConfig>> GetUsers()
-        {
-            return await _userService.GetUsers();
-        }
-
-        /// <summary>
-        /// Get user configuration from ClickSphere.users
-        /// </summary>
-        /// <param name="userId">The user id of the user</param>
-        /// <returns>The user configuration</returns>
-        [Authorize]
-        [HttpGet]
-        [Route("/getUserConfig")]
-        public async Task<IResult?> GetUserConfig(string userId)
-        {
-            if(Guid.TryParse(userId, out Guid uid))
-            {
-                var result = await _userService.GetUserConfig(uid);
-                if (result != null)
-                    return Results.Ok(result);
-                else
-                    return Results.BadRequest("User does not exist");
-            }
-            else
-            {
-                return Results.BadRequest("User ID has to be a valid GUID");
-            }
-        }
-
-        /// <summary>
-        /// Update user configuration.
-        /// </summary>
-        /// <param name="user">The user configuration to update.</param>
-        /// <returns>The result of the update.</returns>
-        [Authorize]
-        [HttpPost]
-        [Route("/updateUser")]
-        public async Task<IResult> UpdateUser([FromBody] UserConfig user)
-        {
-            if (user == null)
-                return Results.BadRequest("User is required");
-
-            Result result = await _userService.UpdateUser(user);
-            if (result.IsSuccessful)
-                return Results.Ok();
-            else
-                return Results.BadRequest(result.Output);
-        }
+        Result result = await _userService.UpdateUser(user);
+        if (result.IsSuccessful)
+            return Results.Ok();
+        else
+            return Results.BadRequest(result.Output);
     }
 }
